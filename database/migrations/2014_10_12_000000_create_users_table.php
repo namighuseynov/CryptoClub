f6467cc4d730bc5fb7f7772d0ca2048f30185ac3
namikguseinoff <namik.guseynov@inbox.ru>
2022-09-10 11:29:22 +0400

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
